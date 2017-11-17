18c458d7374ca783e7eef08f3b1e2e12a7efccb5
taruncse <tarun.pstu@gmail.com>
2017-11-18 00:24:17 +0600

    Renamed the package and rearranged the folders

--- app/src/main/java/boilerplate/tkb/com/boilerplate/utils/SystemUtils.java
+++
@@ -1,9 +0,0 @@
-package boilerplate.tkb.com.boilerplate.utils;
-
-import android.content.res.Resources;
-
-public class SystemUtils {
-    public static int getScreenOrientation() {
-        return Resources.getSystem().getConfiguration().orientation;
-    }
-}
