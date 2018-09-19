b44ba93ecac54382d2641c00672d477ee5e772fc
Tarun <tarun.pstu@gmail.com>
2018-09-19 23:49:05 +0600

    Clean up ignored files

---
+++ app/src/main/java/com/tkb/boilerplate/annotation/AnnotationActivity.java
@@ -0,0 +1,14 @@
+package com.tkb.boilerplate.annotation;
+import android.os.Bundle;
+
+import com.tkb.boilerplate.R;
+import com.tkb.boilerplate.common.BaseActivity;
+
+public class AnnotationActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_annotation);
+    }
+}
