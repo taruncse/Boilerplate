be95c07ee17426e0cb1b8b1fd557c2002c2852f4
Tarun <tarun.pstu@gmail.com>
2018-09-24 23:21:16 +0600

    1. Configured basic format of recycler view

--- app/src/main/java/com/tkb/boilerplate/annotation/AnnotationAdapter.java
+++ app/src/main/java/com/tkb/boilerplate/annotation/AnnotationAdapter.java
@@ -1,4 +1,63 @@
 package com.tkb.boilerplate.annotation;
 
-public class AnnotationAdapter {
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.tkb.boilerplate.R;
+
+import java.util.ArrayList;
+
+public class AnnotationAdapter extends RecyclerView.Adapter<AnnotationAdapter.MyViewHolder> {
+    private ArrayList<DataModel> dataSet;
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.annotation_normal_cards_layout, parent, false);
+
+         view.setOnClickListener(AnnotationNormalActivity.myOnClickListener);
+
+        MyViewHolder myViewHolder = new MyViewHolder(view);
+        return myViewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int listPosition) {
+        TextView textViewName = holder.textViewName;
+        TextView textViewVersion = holder.textViewVersion;
+        ImageView imageView = holder.imageViewIcon;
+
+        textViewName.setText(dataSet.get(listPosition).getName());
+        textViewVersion.setText(dataSet.get(listPosition).getVersion());
+        imageView.setImageResource(dataSet.get(listPosition).getImage());
+    }
+
+    @Override
+    public int getItemCount() {
+        return dataSet.size();
+    }
+
+    public static class MyViewHolder extends RecyclerView.ViewHolder {
+
+        TextView textViewName;
+        TextView textViewVersion;
+        ImageView imageViewIcon;
+
+        public MyViewHolder(View itemView) {
+            super(itemView);
+            this.textViewName = (TextView) itemView.findViewById(R.id.textViewName);
+            this.textViewVersion = (TextView) itemView.findViewById(R.id.textViewVersion);
+            this.imageViewIcon = (ImageView) itemView.findViewById(R.id.imageView);
+        }
+    }
+
+    public AnnotationAdapter(ArrayList<DataModel> data) {
+        this.dataSet = data;
+    }
 }
