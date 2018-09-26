d127e1369e11bd98bdc0d3057f55e7ddae61290d
das.shekhar@northsouth.edu <@53620779tapuDas>
2018-09-26 21:53:47 +0600

    BUTTERKNIFE library with package added

--- app/src/main/java/com/tkb/boilerplate/annotation/AnnotationActivity.java
+++ app/src/main/java/com/tkb/boilerplate/annotation/AnnotationActivity.java
@@ -7,6 +7,9 @@ import android.support.constraint.ConstraintLayout;
 import com.tkb.boilerplate.R;
 import com.tkb.boilerplate.common.BaseActivity;
 
+import org.androidannotations.annotations.AfterViews;
+import org.androidannotations.annotations.ViewById;
+
 
 @EActivity(R.layout.activity_annotation)
 public class AnnotationActivity extends BaseActivity {
---
+++ app/src/main/java/com/tkb/boilerplate/butterknife/butterknifeActivity.java
@@ -0,0 +1,15 @@
+package com.tkb.boilerplate.butterknife;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.tkb.boilerplate.R;
+
+public class butterknifeActivity extends AppCompatActivity {
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_butterknife);
+    }
+}
