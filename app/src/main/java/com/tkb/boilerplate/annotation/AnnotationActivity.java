24b496ba3f3c103e20d1cc3ba66ceef6df9f93e9
das.shekhar@northsouth.edu <@53620779tapuDas>
2018-10-04 18:41:56 +0600

    block annotation please add this activity have some error

--- app/src/main/java/com/tkb/boilerplate/annotation/AnnotationActivity.java
+++ app/src/main/java/com/tkb/boilerplate/annotation/AnnotationActivity.java
@@ -1,29 +1,26 @@
-package com.tkb.boilerplate.annotation;
-
-
-import android.support.constraint.ConstraintLayout;
-
-
-import com.tkb.boilerplate.R;
-import com.tkb.boilerplate.common.BaseActivity;
-
-import org.androidannotations.annotations.AfterViews;
-import org.androidannotations.annotations.ViewById;
-
-
-@EActivity(R.layout.activity_annotation)
-public class AnnotationActivity extends BaseActivity {
-
-    @ViewById
-    ConstraintLayout main;
-
-    @AfterViews
-    void afterViews() {
-
-    }
-   /* @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_annotation);
-    }*/
-}
+//package com.tkb.boilerplate.annotation;
+//
+//
+//import android.support.constraint.ConstraintLayout;
+//import com.tkb.boilerplate.R;
+//import com.tkb.boilerplate.common.BaseActivity;
+//import org.androidannotations.annotations.AfterViews;
+//import org.androidannotations.annotations.ViewById;
+//
+//
+//@EActivity(R.layout.activity_annotation)
+//public class AnnotationActivity extends BaseActivity {
+//
+//    @ViewById
+//    ConstraintLayout main;
+//
+//    @AfterViews
+//    void afterViews() {
+//
+//    }
+//   /* @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_annotation);
+//    }*/
+//}
--- app/src/main/java/com/tkb/boilerplate/butterknife/butterknifeActivity.java
+++ app/src/main/java/com/tkb/boilerplate/butterknife/butterknifeActivity.java
@@ -1,15 +1,32 @@
 package com.tkb.boilerplate.butterknife;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.EditText;
+
 
 import com.tkb.boilerplate.R;
+import com.tkb.boilerplate.common.BaseActivity;
 
-public class butterknifeActivity extends AppCompatActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+
+public class butterknifeActivity extends BaseActivity {
+    
+    @BindView(R.id.username) EditText username;
+    @BindView(R.id.password) EditText password;
     
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_butterknife);
+        ButterKnife.bind(this);
+        
+        
+      
+        
+        
+        
     }
 }
